c0f15f5175de1f975450314a39831f9409764e13
IceBear <69987100+OxIceBear@users.noreply.github.com>
2023-12-22 20:05:39 +0100

    Magic

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
-apt-get update && apt-get upgrade
-apt install software-properties-common
-add-apt-repository ppa:openjdk-r/ppa -y && apt update -y
-apt install openjdk-8-jre
+apt update -y && apt upgrade -y
+apt install sudo -y
+apt install curl -y 
 apt install wget -y
-apt install curl -y
+apt install software-properties-common -y
+add-apt-repository ppa:openjdk-r/ppa -y && apt update -y
+apt install openjdk-8-jre -y
+
+
 curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
-sudo apt update
-sudo apt install playit
+sudo apt update -y
+sudo apt install playit -y
