0cbc44961ecfe13966e74356d41bb6668b62d6a5
IceBear <69987100+OxIceBear@users.noreply.github.com>
2023-12-25 20:29:50 +0100

    Update pwdmethod.sh

--- pwdmethod.sh
+++ pwdmethod.sh
@@ -1,13 +1,15 @@
-apt update && apt upgrade
+apt update
+apt upgrade -y
 apt install sudo 
-apt install curl 
-apt install wget 
-apt install software-properties-common 
-add-apt-repository ppa:openjdk-r/ppa && apt update 
-apt install openjdk-8-jre
+apt install curl -y
+apt install wget -y
+apt install software-properties-common -y
+add-apt-repository ppa:openjdk-r/ppa -y
+apt update 
+apt install openjdk-8-jre -y
 
 
 curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
 sudo apt update
-sudo apt install playit
+sudo apt install playit -y
